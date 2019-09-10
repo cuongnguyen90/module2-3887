7864cf1ee95c8bf9894dbbe88742f0be744dfc8b
Cuong Nguyen <cuongnmph04158@gmail.com>
2019-09-10 15:23:15 +0700

    Upde test.php

--- test.php
+++ test.php
@@ -4,13 +4,14 @@ require_once "Rectangle.php";
 require_once "Circle.php";
 
 
-$shapes[0] = new Circle('hinh tron',9);
+$shapes[0] = new Circle('hinh tron',20);
 $shapes[1] = new Square('hinh vuong',10);
 $shapes[2] = new Rectangle('hinh chu nhat',10,8);
 
 foreach ($shapes as $shape){
+   echo $shape->name."<br>";
    if ($shape instanceof Colorable){
       echo $shape->howToColor()."<br>";
    }
-   echo $shape->calculateArea()."<br>";
+   echo 'S : '.$shape->calculateArea()."<br>";
 }
